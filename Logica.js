a422bea2ebd2d4f9413346e7330130b47faea014
Mealvarezm <enriquemerlos49@gmail.com>
2023-06-05 23:57:35 -0600

    correcion

--- Logica.js
+++ Logica.js
@@ -12,7 +12,7 @@ const forbiddenElements = ['s','a', '!', '"', '#', '$', 'b',
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','a', 'b', 'c', 'd',
  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
  's', 't', 'u', 'v', 'w','`','!','#','$','&','°','_','¡','1','2','3','4',
- '5','6','7','8','9','´','otro-elemento'];
+ '5','6','8','9','´','otro-elemento'];
 
 function parseExpression() {
   const expression = document.getElementById('expression').value;
